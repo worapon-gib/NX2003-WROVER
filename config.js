979dd0ea0e2e5a1baa981fd5a4303d87ac7cba19
worapon-gib <57093784+worapon-gib@users.noreply.github.com>
2020-08-11 09:17:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    name : 'nx2003-wrover',
+    name : 'NX2003-WROVER',
     platform : 'arduino-esp32',
     title : 'NX2003 WROWER-B',
     description : 'NEXPIE 16MB Flash 8 MB PSRAM.\n',
     author : 'Chatpeth Kenanan',
     website : 'https://www.espressif.com/sites/default/files/documentation/esp32-wrover-b_datasheet_en.pdf',
     email : 'ch.ee.psu@outlook.com',
-    git : 'https://github.com/chp-lab/nx2003-kidbright/',
+    git : 'https://github.com/worapon-gib/NX2003-WROVER/',
     image : '/static/FRONT BOARD.png',
     version : '1.1.0'
 };
